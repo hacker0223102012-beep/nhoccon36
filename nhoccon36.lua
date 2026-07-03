16784bab8edbd0746d5c79d6161b092403d78f27
hacker0223102012-beep <hacker0223102012@gmail.com>
2026-07-03 12:28:47 +0700

    Update nhoccon36.lua

--- nhoccon36.lua
+++ nhoccon36.lua
@@ -1,5 +1,7 @@
-for _, v in pairs(game.ReplicatedStorage:GetDescendants()) do
-    if v:IsA("RemoteEvent") then
-        print("Tên Remote: " .. v.Name)
-    end
+-- Thử gửi lệnh "troll" xem hệ thống có nhận không
+local Cmdr = game.ReplicatedStorage:FindFirstChild("CmdrEvent")
+if Cmdr then
+    -- Thử chạy lệnh cộng tiền (Nếu nó dùng Cmdr chuẩn)
+    Cmdr:FireServer("addmoney", 999999) 
+    print("✅ Đã bắn lệnh qua Cmdr!")
 end
